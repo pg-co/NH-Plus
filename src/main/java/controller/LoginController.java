89b6e1f225c5479da7a73d76edcf74c2ca02cc17
Yven <67788726+theraphosida@users.noreply.github.com>
2021-07-02 17:37:53 +0200

    Authentication #4 (#8), close #4
    * Login button added to main window view #4
    
    * Example for conditional login fields #4
    
    * User session and logout added #4
    
    * Logout on main window navigation #4
    
    * User test data removed, check with user table added #4
    
    * removed UserDAO Sysout
    
    Co-authored-by: Pascal Genz <pg@capricorn.de>

---
+++ src/main/java/controller/LoginController.java
@@ -0,0 +1,81 @@
+package controller;
+
+import datastorage.DAOFactory;
+import datastorage.UserDAO;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.PasswordField;
+import model.User;
+
+import java.io.*;
+import java.sql.SQLException;
+import java.util.List;
+
+public class LoginController {
+    @FXML
+    Button btnLogin;
+    @FXML
+    TextField txtUsername;
+    @FXML
+    PasswordField txtPassword;
+    @FXML
+    Label lblAccess;
+    String dbUsername, dbPassword, username, password, dbRolename;
+
+    public void initialize() throws IOException {
+        handleShowLogin();
+    }
+
+    public void handleShowLogin() throws IOException {
+        username = this.txtUsername.getText();
+        password = this.txtPassword.getText();
+
+        // get user data from database
+        UserDAO uDao = DAOFactory.getDAOFactory().createUserDAO();
+        List<User> allUsers;
+
+        // check if input fields are filled
+        if(!username.equals("") && !password.equals("")) {
+            // check if inputs equal database data
+            try {
+                allUsers = uDao.readAll();
+                for (User u : allUsers) {
+                    dbUsername = u.getUsername();
+                    dbPassword = u.getPassword();
+                    dbRolename = u.getRolename();
+
+                    if(username.equals(dbUsername) && password.equals(dbPassword)) {
+                        handleLogin(dbRolename);
+                        break;
+                    }
+                    // print text message if wrong input data
+                    else {
+                        this.lblAccess.setText("Anmeldung fehlgeschlagen! Versuchen Sie es erneut.");
+                    }
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void handleLogin(String rolename) throws IOException {
+        // print text message if login success
+        this.lblAccess.setText("Erfolgreich angemeldet!");
+        // write user session data to file
+        FileWriter usersession = new FileWriter("src/main/usersession.txt");
+        usersession.write(rolename);
+        usersession.close();
+        // set button label
+        MainWindowController.Btn.setValue("Abmelden");
+    }
+
+    protected void handleLogout() throws IOException {
+        // delete user session data from file
+        FileWriter usersession = new FileWriter("src/main/usersession.txt");
+        usersession.write("");
+        usersession.close();
+    }
+}
--- src/main/java/controller/MainWindowController.java
+++ src/main/java/controller/MainWindowController.java
@@ -1,15 +1,40 @@
 package controller;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableStringValue;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
+import sun.jvm.hotspot.utilities.Observable;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 
 public class MainWindowController {
 
     @FXML
     private BorderPane mainBorderPane;
+    @FXML
+    public Button lblBtn = new Button();
+    public static SimpleStringProperty Btn = new SimpleStringProperty("Anmelden");
+    private ObservableStringValue txt = Btn;
+
+    @FXML
+    private void initialize() throws IOException {
+        txt.addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                lblBtn.setText(newValue);
+            }
+        });
+    }
+
 
     @FXML
     private void handleShowAllPatient(ActionEvent e) {
@@ -32,6 +57,7 @@ public class MainWindowController {
         }
         AllTreatmentController controller = loader.getController();
     }
+
     @FXML
     private void handleShowAllUsers(ActionEvent e) {
         FXMLLoader loader = new FXMLLoader(Main.class.getResource("/AllUserView.fxml"));
@@ -41,5 +67,25 @@ public class MainWindowController {
             ex.printStackTrace();
         }
         AllUserController controller = loader.getController();
+    }
+
+    @FXML
+    private void handleShowLogin(ActionEvent e) throws IOException {
+        LoginController lc = new LoginController();
+        // check what button label is set
+        if(this.lblBtn.getText().equals("Abmelden")){
+            lc.handleLogout();
+            // set button label
+            Btn.setValue("Anmelden");
+        }
+        else {
+            FXMLLoader loader = new FXMLLoader(Main.class.getResource("/LoginView.fxml"));
+            try {
+                mainBorderPane.setCenter(loader.load());
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+            LoginController controller = loader.getController();
+        }
     }
 }
